a9e035e2736de1b65fdaaefde6993544f6ab9b08
Lautaro <santucholautaro437@gmail.com>
2023-04-01 14:09:14 -0300

    añadida funcion de login

--- Autenticacion/urls.py
+++ Autenticacion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import viewRegistro, cerrarSesion
+from .views import viewRegistro, cerrarSesion, loguear
 
 
 urlpatterns = [
     path('', viewRegistro.as_view(), name="Autenticacion"),
     path('cerrarSesion', cerrarSesion, name="cerrar_sesion"),
+    path('logear', loguear, name="loguear"),
 
 ]
--- Autenticacion/views.py
+++ Autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # Create your views here.
@@ -29,3 +29,22 @@ def cerrarSesion(request):
     logout(request)
 
     return redirect ("Home")
+
+def loguear(request):
+    if request.method=="POST":
+        form= AuthenticationForm(request, data= request.POST)
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get("username")
+            contrasenia=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombre_usuario, password=contrasenia)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect ("Home")
+            else:
+                messages.error(request, "Usuario invalido")
+        else:
+            messages.error(request, "Informacion invalida")
+
+    form= AuthenticationForm()
+
+    return render(request, "login/login.html", {"form":form})
