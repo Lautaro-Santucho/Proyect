0b31d6191a085fe942c37946bc64a2db33875d73
Lautaro <santucholautaro437@gmail.com>
2023-04-07 14:03:49 -0300

    agregacion de admin en Pedidos y boton de compra

--- Pedidos/admin.py
+++ Pedidos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Pedidos, lineaPedidos
 
 # Register your models here.
+
+class pedidosAdmin(admin.ModelAdmin):
+    readonly_fields=("created_at", )
+
+class lineaPedidosAdmin(admin.ModelAdmin):
+    readonly_fields=("created_at", )
+
+admin.site.register(Pedidos, pedidosAdmin)
+admin.site.register(lineaPedidos, lineaPedidosAdmin)
